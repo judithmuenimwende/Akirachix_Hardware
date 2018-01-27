0084df103c31c12cc733377aca95cd650ae349bd
Judith Mueni <judithmueni395@gmail.com>
2018-01-27 17:22:29 +0300

    first commit

---
+++ Judith_cpp.cpp
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+int addition (int x, int y) {
+int z= x+y;
+return z;
+}
+{
+    int a;
+    int b;
+    cout<<" Enter first number";
+    cin>> a;
+    cout<<"Enter second number";
+    cin>> b;
+    cout<< a; cout<<" + "; cout<< b; cout<< " = "; cout<< additin (a,b);
+
+    return 0;
+}
+
+
+
